971cdf950ff8e937d1574c36f2b419428357a053
ali06avci <ali06avci@gmail.com>
2023-01-02 19:44:43 +0300

    expenseform updated

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,19 +5,59 @@ const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+  /*  const [userInput, setuserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  }); */
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
+
+    //setuserInput({ ...userInput, enteredTitle: event.target.value });
+
+    /*  setuserInput((prevState) => {
+      return { ...prevState, enteredTitle: event.target.value };
+    }); */
   };
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
+
+    /* setuserInput({
+      ...userInput,
+      enteredAmount: event.target.value,
+    }); */
+
+    /*  setuserInput((prevState) => {
+      return { ...prevState, enteredAmount: event.target.value };
+    }); */
   };
   const dateChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
+    setEnteredDate(event.target.value);
+
+    /* setuserInput({
+      ...userInput,
+      enteredDate: event.target.value,
+    }); */
+
+    /* setuserInput((prevState) => {
+      return { ...prevState, enteredDate: event.target.value };
+    }); */
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
@@ -37,7 +77,7 @@ const ExpenseForm = () => {
           <input
             type="date"
             min="2019-01-01"
-            max="2022-12-31"
+            max="2023-12-31"
             onChange={dateChangeHandler}
           />
         </div>
